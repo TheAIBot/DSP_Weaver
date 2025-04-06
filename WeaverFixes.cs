d725dea5f46cfa01de140f2a0b3102128d66269c
TheAIBot <andreasuranus@hotmail.com>
2025-04-06 15:17:42 +0200

    Preparation for 1.0.1

--- WeaverFixes.cs
+++ WeaverFixes.cs
@@ -10,7 +10,7 @@ internal static class ModInfo
 {
     public const string Guid = "Weaver";
     public const string Name = "Weaver";
-    public const string Version = "0.0.1";
+    public const string Version = "1.0.1";
 }
 
 [BepInPlugin(ModInfo.Guid, ModInfo.Name, ModInfo.Version)]
