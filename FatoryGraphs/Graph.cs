952ab592de00ec7fa37a1406edc2a751b797990c
TheAIBot <andreasuranus@hotmail.com>
2025-04-20 17:47:35 +0200

    Remove entity id from graph since it was not used. Use `EntityTypeIndex` as the key to look up nodes instead

--- FatoryGraphs/Graph.cs
+++ FatoryGraphs/Graph.cs
@@ -4,14 +4,14 @@ namespace Weaver.FatoryGraphs;
 
 internal sealed class Graph
 {
-    private readonly Dictionary<int, Node> _idToNode = [];
+    private readonly Dictionary<EntityTypeIndex, Node> _entityTypeIndexToNode = [];
 
-    public int NodeCount => _idToNode.Count;
+    public int NodeCount => _entityTypeIndexToNode.Count;
 
     public void AddNode(Node node)
     {
-        _idToNode.Add(node.EntityId, node);
+        _entityTypeIndexToNode.Add(node.EntityTypeIndex, node);
     }
 
-    public IEnumerable<Node> GetAllNodes() => _idToNode.Values;
+    public IEnumerable<Node> GetAllNodes() => _entityTypeIndexToNode.Values;
 }
