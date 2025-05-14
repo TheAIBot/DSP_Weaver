90280e745383f772555147707b99a8052c12139d
TheAIBot <andreasuranus@hotmail.com>
2025-05-14 17:59:43 +0200

    Lets make sure researched tech is always removed from queue. Just to be safe.

--- Optimizations/LinearDataAccess/Labs/StarClusterResearchManager.cs
+++ Optimizations/LinearDataAccess/Labs/StarClusterResearchManager.cs
@@ -20,43 +20,49 @@ internal sealed class StarClusterResearchManager
     public void UIThreadUnlockResearchedTechnologies(GameHistoryData history)
     {
         bool hasResearchedAnything = false;
-        foreach (ResearchedTech technology in _tickResearchedTech)
+        try
         {
-            if (technology.State.unlocked)
+            foreach (ResearchedTech technology in _tickResearchedTech)
             {
-                hasResearchedAnything = true;
-                history.techStates[technology.ResearchTechId] = technology.State;
-                for (int l = 0; l < technology.Proto.UnlockRecipes.Length; l++)
+                if (technology.State.unlocked)
                 {
-                    history.UnlockRecipe(technology.Proto.UnlockRecipes[l]);
+                    hasResearchedAnything = true;
+                    history.techStates[technology.ResearchTechId] = technology.State;
+                    for (int l = 0; l < technology.Proto.UnlockRecipes.Length; l++)
+                    {
+                        history.UnlockRecipe(technology.Proto.UnlockRecipes[l]);
+                    }
+                    for (int m = 0; m < technology.Proto.UnlockFunctions.Length; m++)
+                    {
+                        history.UnlockTechFunction(technology.Proto.UnlockFunctions[m], technology.Proto.UnlockValues[m], technology.PreviousTechLevel);
+                    }
+                    for (int n = 0; n < technology.Proto.AddItems.Length; n++)
+                    {
+                        history.GainTechAwards(technology.Proto.AddItems[n], technology.Proto.AddItemCounts[n]);
+                    }
+                    history.NotifyTechUnlock(technology.ResearchTechId, technology.PreviousTechLevel);
                 }
-                for (int m = 0; m < technology.Proto.UnlockFunctions.Length; m++)
+                if (technology.State.curLevel > technology.PreviousTechLevel)
                 {
-                    history.UnlockTechFunction(technology.Proto.UnlockFunctions[m], technology.Proto.UnlockValues[m], technology.PreviousTechLevel);
+                    hasResearchedAnything = true;
+                    history.techStates[technology.ResearchTechId] = technology.State;
+                    for (int num6 = 0; num6 < technology.Proto.UnlockFunctions.Length; num6++)
+                    {
+                        history.UnlockTechFunction(technology.Proto.UnlockFunctions[num6], technology.Proto.UnlockValues[num6], technology.PreviousTechLevel);
+                    }
+                    for (int num7 = 0; num7 < technology.Proto.AddItems.Length; num7++)
+                    {
+                        history.GainTechAwards(technology.Proto.AddItems[num7], technology.Proto.AddItemCounts[num7]);
+                    }
+                    history.NotifyTechUnlock(technology.ResearchTechId, technology.PreviousTechLevel);
                 }
-                for (int n = 0; n < technology.Proto.AddItems.Length; n++)
-                {
-                    history.GainTechAwards(technology.Proto.AddItems[n], technology.Proto.AddItemCounts[n]);
-                }
-                history.NotifyTechUnlock(technology.ResearchTechId, technology.PreviousTechLevel);
-            }
-            if (technology.State.curLevel > technology.PreviousTechLevel)
-            {
-                hasResearchedAnything = true;
-                history.techStates[technology.ResearchTechId] = technology.State;
-                for (int num6 = 0; num6 < technology.Proto.UnlockFunctions.Length; num6++)
-                {
-                    history.UnlockTechFunction(technology.Proto.UnlockFunctions[num6], technology.Proto.UnlockValues[num6], technology.PreviousTechLevel);
-                }
-                for (int num7 = 0; num7 < technology.Proto.AddItems.Length; num7++)
-                {
-                    history.GainTechAwards(technology.Proto.AddItems[num7], technology.Proto.AddItemCounts[num7]);
-                }
-                history.NotifyTechUnlock(technology.ResearchTechId, technology.PreviousTechLevel);
             }
         }
-
-        _tickResearchedTech.Clear();
+        // If any of the above fail for some reason then the queue will be cleared so the issue doesn't persist.
+        finally
+        {
+            _tickResearchedTech.Clear();
+        }
 
         if (hasResearchedAnything)
         {
