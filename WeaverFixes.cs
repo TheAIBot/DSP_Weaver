24886bf4779fa414d9974a2ac09905ec6801321e
TheAIBot <andreasuranus@hotmail.com>
2025-05-17 17:58:22 +0200

    Update to 1.2.4

--- WeaverFixes.cs
+++ WeaverFixes.cs
@@ -10,7 +10,7 @@ internal static class ModInfo
 {
     public const string Guid = "Weaver";
     public const string Name = "Weaver";
-    public const string Version = "1.2.3";
+    public const string Version = "1.2.4";
 }
 
 internal static class ModDependencies
