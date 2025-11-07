be50a3928fb0296c8dd440c42e533a2abfb05d4f
TheAIBot <andreasuranus@hotmail.com>
2025-11-07 02:02:50 +0100

    Update to 2.1.2

--- WeaverFixes.cs
+++ WeaverFixes.cs
@@ -18,7 +18,7 @@ internal static class ModInfo
 {
     public const string Guid = "Weaver";
     public const string Name = "Weaver";
-    public const string Version = "2.1.1";
+    public const string Version = "2.1.2";
 }
 
 [BepInPlugin(ModInfo.Guid, ModInfo.Name, ModInfo.Version)]
